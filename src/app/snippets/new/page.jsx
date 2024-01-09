004aa30d760e653f5ecddadb55b446f65d1f3ce1
Athif Binu <binuathif@gmail.com>
2024-01-09 23:40:17 +0530

    add basic ui

--- src/app/snippets/new/page.jsx
+++ src/app/snippets/new/page.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-const SnippetsCreatePage = () => {
+const SnippetsCreate = () => {
   return (
-    <div>
-      create snippets
+    <form>
+    <h3 className="font-bold m-3">Create a Snippet</h3>
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-4">
+        <label className="w-12" htmlFor="title">
+          Title
+        </label>
+        <input
+          name="title"
+          className="border rounded p-2 w-full"
+          id="title"
+        />
+      </div>
+
+      <div className="flex gap-4">
+        <label className="w-12" htmlFor="code">
+          Code
+        </label>
+        <textarea
+          name="code"
+          className="border rounded p-2 w-full"
+          id="code"
+        />
+      </div>
+
+      <button type="submit" className="rounded p-2 bg-blue-200">
+        Create
+      </button>
     </div>
+  </form>
   );
 }
 
-export default SnippetsCreatePage;
+export default SnippetsCreate;
